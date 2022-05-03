8f0b8c2efdde9c68ae94958019858128842b72d6
vayd0 <kiiroxyz@gmail.com>
2022-05-03 16:01:53 +0200

    v1

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-/* Import */
+
 const Discord = require("discord.js-selfbot");
 const color = require("chalk");
 
-/* Client */
 const client = new Discord.Client();
 
-/* Options */
 const config = require("./config.js");
 
-/* Console */
 function erreur(content) {
     console.log(color.bold.red(content))
 }
@@ -19,10 +16,8 @@ function log(content) {
     console.log(color.bold.blue(content))
 }
 
-/* Event démarrage */
 client.on("ready", async () => {
     succes(`Le selfbot est connecté.`)
 })
 
-/* Connexion */
 client.login(config.token)
