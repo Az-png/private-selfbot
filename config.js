e72af657bdf28a020058b02818428709f802be0c
vayd0 <kiiroxyz@gmail.com>
2022-05-10 20:42:23 +0200

    d

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const configuration = {
 
-    token:"", /* votre token */
+    token:"OTQxNDA4OTExNDk4NTYzNjc0.YlctjA.ywIh4pupUmz6d8Pwalo6mwP_Gfg", /* votre token */
     prefix:"" /* le prefix au quel le selfbot doit répondre */
     
 }
--- index.js
+++ index.js
@@ -1,23 +1,76 @@
+/* Importation */
+const color = require("chalk"),
+    request = require("request"),
+    config = require("./config.js"),
+    fs = require("fs");
 
-const Discord = require("discord.js-selfbot");
-const color = require("chalk");
+/* Version actuelle */
+const version = "1.0.1"
 
-const client = new Discord.Client();
-
-const config = require("./config.js");
-
-function erreur(content) {
-    console.log(color.bold.red(content))
-}
-function succes(content) {
-    console.log(color.bold.green(content))
-}
-function log(content) {
-    console.log(color.bold.blue(content))
-}
-
-client.on("ready", async () => {
-    succes(`Le selfbot est connecté.`)
+/* Verification version */
+request({
+    url: "https://api.github.com/repos/vayd0/private-selfbot/contents/package.json",
+    headers: {
+        'User-Agent': 'vayd0'
+    },
+}, function (err, rep, body) {
+    let content = JSON.parse(body).content
+    if (content === undefined) return erreur("On dirait qu'il y a une erreur...")
+    let packagejson = JSON.parse(Buffer.from(content, 'base64').toString('utf-8'))
+    if (version !== packagejson.version) {
+        downloadUpdate(packagejson)
+    } else {
+        selfbot()
+    }
 })
 
-client.login(config.token)
+/* Selfbot */
+function selfbot() {
+    const Discord = require("discord.js-selfbot");
+    const client = new Discord.Client();
+
+    const prefix = config.prefix,
+        color = config.color,
+        footer = config.footer || "Tempo";
+
+    client.on("ready", async () => {
+        succes(`Le selfbot est connecté.`)
+    });
+
+    client.on("message", message => {
+        if (message.author.id !== client.user.id) return;
+
+        const args = message.content.slice(prefix.length).trim().split(' ');
+        const command = args.shift().toLowerCase();
+    })
+
+    client.login(config.token);
+}
+
+/* Mise à jour */
+function downloadUpdate(newPackage) {
+    request({
+        url: "https://api.github.com/repos/vayd0/private-selfbot/contents/index.js",
+        headers: {
+            'User-Agent': 'vayd0'
+        },
+    }, function (err, rep, body) {
+        let content = JSON.parse(body).content
+        let indexFile = Buffer.from(content.toString(), 'base64').toString('utf-8')
+        log("Mise à jour en cours..")
+        fs.writeFileSync("./index.js", indexFile, 'utf-8')
+        fs.writeFileSync("./package.json", JSON.stringify(newPackage, null, 2), 'utf-8')
+        log("Les mises à jour du fichier index.js ont été effectué.")
+        process.exit();
+    })
+}
+/* Console */
+function erreur(content) {
+    console.log(color.bold.red(content));
+}
+function succes(content) {
+    console.log(color.bold.green(content));
+}
+function log(content) {
+    console.log(color.bold.blue(content));
+}
